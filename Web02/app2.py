9843cfc0d0b30e322ebd1f40a15e92d6ed812d01
TheQuyen <quyen.th.bui@gmail.com>
2018-04-05 00:47:45 +0700

    'web02new'

--- Web02/app2.py
+++ Web02/app2.py
@@ -5,6 +5,12 @@ from models.customer import Customer
 app = Flask(__name__)
 mlab.connect()
 
+@app.route('/customer')
+def customer():
+    all_customers = Service.objects()
+    return render_template('search.html', all_services =all_services )
+
+
 @app.route('/customer/male10')
 def male10():
     best_customers = Customer.objects(gender=1,contacted=False)[:10]
